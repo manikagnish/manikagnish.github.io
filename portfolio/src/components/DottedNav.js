0bc7fefaaac7b573123a2457c147aa45aa18012c
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-05 00:01:47 +0530

    added the dotted nav

---
+++ portfolio/src/components/DottedNav.js
@@ -0,0 +1,40 @@
+export default function DottedNav() {
+  return (
+    <nav className="inline-block lg:mr-24 lg:w-4 fixed left-percentage hidden xl:block">
+      <div className="absolute left-50 transform -trasnlate-x-1/2 space-y-6 mt-36">
+        <a
+          href="/#"
+          className="nav-dot selected-circle block w-7 h-7 rounded-full border-4 border-nav bg-body"
+        >
+          <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+            home
+          </span>
+        </a>
+        <a
+          href="/#"
+          className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body"
+        >
+          <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+            work
+          </span>
+        </a>
+        <a
+          href="/#"
+          className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body"
+        >
+          <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+            about
+          </span>
+        </a>
+        <a
+          href="/#"
+          className="nav-dot block w-7 h-7 rounded-full border-4 border-nav bg-body"
+        >
+          <span className="bg-black px-2 py-1 rounded-md ml-10 opacity-0">
+            hire
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+}
--- portfolio/src/components/HeroSection.js
+++ portfolio/src/components/HeroSection.js
@@ -1,3 +1,11 @@
+import DottedNav from "./DottedNav";
+
 export default function HeroSection() {
-  return <div></div>;
+  return (
+    <div className="container mt-16 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+      <div className="flex flex-wrap md:flex-nowrap">
+        <DottedNav />
+      </div>
+    </div>
+  );
 }
