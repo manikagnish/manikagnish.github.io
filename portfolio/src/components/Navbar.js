126e5ce7d3d0500a9b7fce0dff80cd7d9d729d7c
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-04 23:38:53 +0530

    completed the navbar

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,16 +1,34 @@
 export default function Navbar() {
   return (
-    <header>
-      <div className="container">
-        <a href="/#">Manik.agnish</a>
-      </div>
-      <div className="">
-        <a href="/" className="text-selected-text">
-          Home
+    <header className="py-6">
+      <div className="container flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 w-full">
+        <a href="/#" className="text-lg font-bold">
+          Manik.agnish
         </a>
-        <a href="/work">My work</a>
-        <a href="/about">Aboute me</a>
-        <a href="/hire">Hire me</a>
+        <div className="hidden md:flex space-x-12 items-center">
+          <a href="/" className="text-selected-text">
+            Home
+          </a>
+          <a href="/work">My work</a>
+          <a href="/about">Aboute me</a>
+          <a href="/hire">
+            <button className="px-6 py-2 bg-theme font-bold">Hire me</button>
+          </a>
+        </div>
+        <div className="md:hidden">
+          <svg
+            width="26"
+            height="18"
+            viewBox="0 0 26 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13 17.5H0.25V14.6667H13V17.5ZM25.75 10.4167H0.25V7.58333H25.75V10.4167ZM25.75 3.33333H13V0.5H25.75V3.33333Z"
+              fill="white"
+            />
+          </svg>
+        </div>
       </div>
     </header>
   );
